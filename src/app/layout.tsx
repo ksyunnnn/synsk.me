734755ba8b7603278de66b6e04f1b11523838f42
ksyunnnn <is.syunsukekobashi@gmail.com>
2025-02-17 17:04:28 +0900

    update nextjs15

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ const lato = Lato({ subsets: ['latin'], weight: ['100', '300', '400', '700'] });
 export const metadata: Metadata = {
   title: 'synsk.me',
   description: 'SYUNSUKE KOBASHI / 小橋俊介 / こばしゅん',
+
   keywords: [
     'portfolio',
     'synsk',
@@ -21,17 +22,19 @@ export const metadata: Metadata = {
     'React',
     'TypeScript',
   ],
+
   openGraph: {
     type: 'website',
     title: 'synsk.me',
     description: 'SYUNSUKE KOBASHI / 小橋俊介 / こばしゅん',
   },
+
   twitter: {
     card: 'summary_large_image',
     title: 'synsk.me',
     description: 'SYUNSUKE KOBASHI / 小橋俊介 / こばしゅん',
     creator: '@ksyunnnn',
-  },
+  }
 };
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
